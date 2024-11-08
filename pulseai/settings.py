0f7244d9517322566d84fa5f9d93076ca5132067
Nikhil <138752464+Nikhilpaikray2003@users.noreply.github.com>
2024-11-08 13:12:26 +0530

    Add files via upload

--- pulseai/settings.py
+++ pulseai/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8o%^_@th&!_+gv$*k(ao_hxzdco!d9a)ogc^0s@e#6vpj09so+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
